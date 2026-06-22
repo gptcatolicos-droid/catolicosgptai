87d707aa2b526a07f1672002952fcf44fab747b9
gptcatolicos-droid <gptcatolicos@gmail.com>
2026-06-22 08:46:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const advancedEngine = require('./advanced-query-engine');
 const { GoogleGenAI } = require('@google/genai');
 
 const app  = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '30mb' }));
